94445cde735695fcc9121b5803a559779cba8784
abdul-rehman-ship <54790239+abdul-rehman-ship@users.noreply.github.com>
2023-02-15 15:00:27 +0500

    first commit

---
+++ pages/Subscription/index.tsx
@@ -0,0 +1,37 @@
+import axios from "axios"
+import getStripe from '../../get-stripe.js'
+
+export default function index() {
+
+
+
+
+
+    const redirectToCheckout = async () => {
+        // Create Stripe checkout
+        const {
+          data: { id },
+        } = await axios.post('/api/checkout_sessions', {
+          items: {
+            price: 'price_1J9ZQoJZ2Z2Z2Z2Z2Z2Z2Z2Z',
+            quantity: 1,
+          }
+        });
+    
+        // Redirect to checkout
+        const stripe = await getStripe();
+        await stripe.redirectToCheckout({ sessionId: id });
+      };
+    
+    
+    
+  return (
+    <div className='container'>
+        <h1>Subscription</h1>
+        <button onClick={redirectToCheckout}>Subscribe</button>
+
+
+
+    </div>
+  )
+}
